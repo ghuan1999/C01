2a06d78f015618de4320ffc2b3297eba6690bc32
legiahuan0709 <legiahuan0709@gmail.com>
2023-03-09 09:02:49 +0700

    thay doi

--- bai01.ts
+++ bai01.ts
@@ -3,5 +3,5 @@ import {question} from 'readline-sync';
 let a: number=Number(question("Nhap a: "));
 let tram: number=Math.floor(a/100);
 let chuc: number=Math.floor((a-tram*100)/10);
-let donVi: number=a%10;
+let donVi: number=a%10+1;
 console.log(tram, chuc, donVi);
